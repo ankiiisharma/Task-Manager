98a380b9881a5b4b7496a11e66b2ea87460fb02f
Ankiiisharma <myselfankit.2003@gmail.com>
2023-06-18 00:26:31 +0530

    some random chnages

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-const input = document.querySelector('#task-name');
+// const input = document.querySelector('#task-name');
+// const button = document.querySelector('button');
+// const allTasks = document.querySelector('.all-tasks');
+// const clearButton = document.querySelector('.clear');
+
+// button.addEventListener('click', function(){
+//   console.log(input.value);
+//   allTasks.innerHTML += '<h3>' + input.value + '</h3>';
+//   input.value = '';
+// })
+
+
+const input = document.querySelector('#tasks');
 const button = document.querySelector('button');
 const allTasks = document.querySelector('.all-tasks');
-const clearButton = document.querySelector('.clear');
+
 
 button.addEventListener('click', function(){
+
   console.log(input.value);
   allTasks.innerHTML += '<h3>' + input.value + '</h3>';
-  input.value = '';
+  input.value='';
 })
