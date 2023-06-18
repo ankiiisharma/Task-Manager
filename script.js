3f4380fe37810e838e09530a869f75c511625fbd
Ankiiisharma <myselfankit.2003@gmail.com>
2023-06-18 12:26:40 +0530

    clear added

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-// const input = document.querySelector('#task-name');
-// const button = document.querySelector('button');
-// const allTasks = document.querySelector('.all-tasks');
-// const clearButton = document.querySelector('.clear');
-
-// button.addEventListener('click', function(){
-//   console.log(input.value);
-//   allTasks.innerHTML += '<h3>' + input.value + '</h3>';
-//   input.value = '';
-// })
-
-
 const input = document.querySelector('#tasks');
 const button = document.querySelector('button');
 const allTasks = document.querySelector('.all-tasks');
-
+const clear = document.querySelector('.clear-task');
 
 button.addEventListener('click', function(){
 
   console.log(input.value);
   allTasks.innerHTML += '<h3>' + input.value + '</h3>';
   input.value='';
+})
+
+clear.addEventListener('click', function(){
+  allTasks.innerHTML = '';
 })
